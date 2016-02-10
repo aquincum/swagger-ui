a53aab5cdccfcf3013695b04b49616a80c153176
aquincum <daniel@szeredi.hu>
2016-02-10 01:14:14 -0500

    Let's do this

--- gulpfile.js
+++ gulpfile.js
@@ -25,12 +25,14 @@ var banner = ['/**',
   ' */',
   ''].join('\n');
 
+var distFolder = './dist';
+
 /**
  * Clean ups ./dist folder
  */
 gulp.task('clean', function() {
   return gulp
-    .src('./dist', {read: false})
+    .src(distFolder, {read: false})
     .pipe(clean({force: true}))
     .on('error', log);
 });
@@ -68,12 +70,12 @@ function _dist() {
     .pipe(concat('swagger-ui.js'))
     .pipe(wrap('(function(){<%= contents %>}).call(this);'))
     .pipe(header(banner, { pkg: pkg }))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest(distFolder))
     .pipe(uglify())
     .on('error', log)
     .pipe(rename({extname: '.min.js'}))
     .on('error', log)
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest(distFolder))
     .pipe(connect.reload());
 }
 gulp.task('dev-dist', ['lint', 'dev-copy'], _dist);
@@ -105,19 +107,19 @@ function _copy() {
   // copy JavaScript files inside lib folder
   gulp
     .src(['./lib/**/*.{js,map}'])
-    .pipe(gulp.dest('./dist/lib'))
+    .pipe(gulp.dest(distFolder+'/lib'))
     .on('error', log);
 
   // copy `lang` for translations
   gulp
     .src(['./lang/**/*.js'])
-    .pipe(gulp.dest('./dist/lang'))
+    .pipe(gulp.dest(distFolder+'/lang'))
     .on('error', log);
 
   // copy all files inside html folder
   gulp
     .src(['./src/main/html/**/*'])
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest(distFolder))
     .on('error', log);
 }
 gulp.task('dev-copy', ['dev-less', 'copy-local-specs'], _copy);
@@ -126,7 +128,7 @@ gulp.task('copy-local-specs', function () {
   // copy the test specs
   return gulp
     .src(['./test/specs/**/*'])
-    .pipe(gulp.dest('./dist/specs'))
+    .pipe(gulp.dest(distFolder+'/specs'))
     .on('error', log);
 });
 
